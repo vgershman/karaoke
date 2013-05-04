3423fda312f81d3b20b485b1a0892b66731a112a
vgershman <herrschmann@gmail.com>
2013-05-04 22:51:04 +0400

    Start to implement design

---
+++ src/com/expelabs/karaoke/view/CustomFontTextView.java
@@ -0,0 +1,11 @@
+package com.expelabs.karaoke.view;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vadimgershman
+ * Date: 04.05.13
+ * Time: 16:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class CustomFontTextView {
+}
